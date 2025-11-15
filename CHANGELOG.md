8343f958a74b8bf80584e45f485958e2e5e31c83
dosty17 <doste.pshtiwan@gmail.com>
2025-11-15 13:27:45 +0300

    new

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Changelog
 
 All notable changes to this project will be documented in this file.
-## [1.0.1] - 2025-11-15
+## [1.0.2] - 2025-11-15
 - Enhance readme.md
 - Some changes
 
--- lib/best_localization.dart
+++ lib/best_localization.dart
@@ -1,7 +1,7 @@
 export 'src/localization.dart';
 
 // Export loaders
-export 'src/loaders/translation_loader.dart';
+// export 'src/loaders/translation_loader.dart';
 export 'src/loaders/json_loader.dart';
 export 'src/loaders/csv_loader.dart';
 export 'src/loaders/yaml_loader.dart';
